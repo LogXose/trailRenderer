19a82b8f38a251ea7fc928de66cccbe5bc019e0f
LogXose <37669964+LogXose@users.noreply.github.com>
2019-01-10 05:43:44 +0300

    v1

---
+++ New Unity Project (1)/Assets/GameManagger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagger : MonoBehaviour {
+    GameObject LastTrail;
+    public GameObject Prefab;
+    bool isDrawing = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            if (isDrawing)
+            {
+                Plane ObjPlane = new Plane(Camera.main.transform.forward * -1, transform.position);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float rayDist;
+                if (ObjPlane.Raycast(ray, out rayDist))
+                {
+                    LastTrail.transform.position = ray.GetPoint(rayDist);
+                    isDrawing = true;
+                }
+            }
+            else
+            {
+                
+                Plane ObjPlane = new Plane(Camera.main.transform.forward * -1, transform.position);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float rayDist;
+                if (ObjPlane.Raycast(ray, out rayDist))
+                {
+                    GameObject gameObject = Instantiate(Prefab, ray.GetPoint(rayDist),Quaternion.identity);
+                    LastTrail = gameObject;
+                    isDrawing = true;
+                }
+            }
+        }
+        else
+        {
+            isDrawing = false;
+        }
+	}
+}
